d3491009ab4b9a6d2172928df650751197b6da64
José Antonio Carmona <e.jacarmona@go.ugr.es>
2024-12-23 17:52:19 +0100

    GaelGoncalba#8, estructura del producto

---
+++ internal/producto.go
@@ -0,0 +1,35 @@
+package internal
+
+import "errors"
+
+type Producto struct {
+	Nombre       string
+	Marca        string
+	Precio       float64
+	Supermercado string
+	AptoPara     []TipoRestriccion //Etiquetas para alergias o restricciones
+}
+
+func NuevoProducto(nombre, marca string, precio float64, supermercado string, aptoPara []string) Producto {
+	return Producto{
+		Nombre:       nombre,
+		Marca:        marca,
+		Precio:       precio,
+		Supermercado: supermercado,
+		AptoPara:     aptoPara,
+	}
+}
+
+type TipoRestriccion string
+
+const (
+	SinGluten       TipoRestriccion = "SinGluten"
+	SinLactosa      TipoRestriccion = "SinLactosa"
+	Vegetariano     TipoRestriccion = "Vegetariano"
+	Vegano          TipoRestriccion = "Vegano"
+	SinFrutosSecos  TipoRestriccion = "SinFrutosSecos"
+	SinHuevos       TipoRestriccion = "SinHuevos"
+	SinMariscos     TipoRestriccion = "SinMariscos"
+	SinSoja         TipoRestriccion = "SinSoja"
+	SinPescado      TipoRestriccion = "SinPescado"
+)
