d44017d2a806c465942ac5df561d2fb712a072c0
José Antonio Carmona <e.jacarmona@go.ugr.es>
2025-01-05 10:48:27 +0100

    #10, corrección "Alergia" y presupuesto negativo

--- internal/usuario.go
+++ internal/usuario.go
@@ -4,7 +4,19 @@ type Usuario struct {
 	DNI           string
 	Nombre        string
 	Presupuesto   float64
-	Alergias      []TipoRestriccion
+	Alergias      []Alergia
 	Supermercados []Supermercado
 }
 
+func NuevoUsuario(dni, nombre string, presupuesto float64, alergias []Alergia, supermercados []Supermercado) (Usuario, error) {
+    if presupuesto < 0 {
+        return Usuario{}, errors.New("El presupuesto no puede ser negativo")
+    }
+    return Usuario{
+        DNI:           dni,
+        Nombre:        nombre,
+        Presupuesto:   presupuesto,
+        Alergias:      alergias,
+        Supermercados: supermercados,
+    }, nil
+}
