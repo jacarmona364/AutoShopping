0eab8d3d16bcca00bc8c6f0ef8b4ff19882c25ff
José Antonio Carmona <e.jacarmona@go.ugr.es>
2025-01-07 23:12:37 +0100

    #9, estructura del producto y tipos de alergia

---
+++ internal/producto.go
@@ -0,0 +1,37 @@
+package internal
+
+import "errors"
+
+type Producto struct {
+	Nombre       string
+	Marca        string
+	Precio       float64
+	Supermercado Supermercado
+	Alergias     []Alergia
+}
+
+type Alergia string const (
+	SinGluten       Alergia = "SinGluten"
+	SinLactosa      Alergia = "SinLactosa"
+	Vegetariano     Alergia = "Vegetariano"
+	Vegano          Alergia = "Vegano"
+	SinFrutosSecos  Alergia = "SinFrutosSecos"
+	SinHuevos       Alergia = "SinHuevos"
+	SinMariscos     Alergia = "SinMariscos"
+	SinSoja         Alergia = "SinSoja"
+	SinPescado      Alergia = "SinPescado"
+)
+
+func NuevoProducto(nombre, marca string, precio float64, supermercado Supermercado, alergias []Alergia) Producto {
+	if precio < 0 {
+        return Producto{}, errors.New("El precio del producto no puede ser negativo")
+    }
+
+	return Producto{
+		Nombre:       nombre,
+		Marca:        marca,
+		Precio:       precio,
+		Supermercado: supermercado,
+		Alergias:     alergias,
+	}
+}
